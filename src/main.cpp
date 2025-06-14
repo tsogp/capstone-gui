7a57b748432ed137821689efa483d664c3c1c45a
Potemkin Pavel <potemkinpavel3@gmail.com>
2025-06-14 23:53:01 +0300

    feat: made test env and set up first test

--- src/main.cpp
+++ src/main.cpp
@@ -4,21 +4,10 @@
 #include <QQmlApplicationEngine>
 #include <QDirIterator>
 #include <QDebug>
-
-void printResources() {
-    qDebug() << "Available resources under :/FullScreen3DView:";
-    QDirIterator it(":/FullScreen3DView", QDirIterator::Subdirectories);
-    while (it.hasNext()) {
-        qDebug() << it.next();
-    }
-}
-
 #include <QGuiApplication>
 
 int main(int argc, char *argv[]) {
     QGuiApplication app(argc, argv);
-
-    printResources();
 
     MainWindow mainWindow;
     mainWindow.loadMainQml();
